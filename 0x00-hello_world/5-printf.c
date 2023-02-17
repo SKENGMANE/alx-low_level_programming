f6ae8982e02ccc894e103ad0231b90315fce4768
vagrant <abboluwatife1@gmail.com>
2023-02-17 04:21:08 +0000

    printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * A C program that prints with printf not puts
+ * Return: Always 0 (Success)
+ */
+int main(void) 
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
+
+
