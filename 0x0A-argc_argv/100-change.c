f921a070d32283fec9f620d53c9931199edc5d3d
vagrant <abboluwatife1@gmail.com>
2023-03-14 02:41:21 +0000

    change for coins

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - entry point
+ *
+ * this program prints the minimun numbers
+ * of coins to make change for an amount of money *
+ *
+ * @argc: number of cli arguments
+ * @argv: list of cli arguments
+ *
+ * Return: 1 if number of arguments passed is not 1, else 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int cents = 0;
+	int num_coins = 0;
+	int coins[5] = {25, 10, 5, 2, 1};
+	int i;
+	
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	if (cents < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	 cents = atoi(argv[1]);
+
+	for (i = 0; cents && i < 5; i++)
+	{
+		num_coins += cents / coins[i];
+		cents %= coins[i];
+	}
+
+	printf("%d\n", num_coins);
+	return (0);
+}
